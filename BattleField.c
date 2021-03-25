b06b1a1bee77eba03dfe65cd2ba59991dc2cb78b
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-03-25 16:38:31 +0200

    Update BattleField.c

--- BattleField.c
+++ BattleField.c
@@ -1,41 +1,12 @@
 #include "BattleField.h"
 #include "Vector.h"
 #include "Defines.h"
+#include "Airship.h"
 #include <stdio.h>
 #include <stdlib.h>
-void initAirship(char c, Airship* ship){
-   if(c == 'v'){
-      ship->type = VIKING;
-      ship->health = VIKING_HEALTH;
-      ship->shield = 0;
-      ship->shieldRegenerateRate = 0;
-      ship->demage = VIKING_DAMAGE;
-    }
-    else if (c =='b'){
-      ship->type = BATTLE_CRUSER;
-      ship->health = BATTLE_CRUSER_HEALTH;
-      ship->shield = 0;
-      ship->shieldRegenerateRate = 0;
-      ship->demage = BATTLE_BRUSER_DAMAGE;
-    }
-    else if(c == 'p'){
-      ship->type = PHOENIX;
-      ship->health = PHOENIX_HEALTH;
-      ship->shield = PHOENIX_SHIELD;
-      ship->shieldRegenerateRate = PHOENIX_SHIELD_REGENERATE_RATE;
-      ship->demage = PHOENIX_DAMAGE;
-    }
-    else if (c == 'c'){
-      ship->type = CARRIER;
-      ship->health = CARRIER_HEALTH;
-      ship->shield = CARRIER_SHIELD;
-      ship->shieldRegenerateRate = CARRIER_SHIELD_REGENERATE_RATE;
-      ship->demage = CARRIER_DAMAGE;
-    }
 
-}
 void generateTerranFleet(BattleField *battleField, const char *terranFleetStr) {
-  vectorInit(&battleField->terranFleet, sizeof(Vector));
+  vectorInit(&battleField->terranFleet, strlen(terranFleetStr));
   int i = 0;
   while (terranFleetStr[i] != '\0')
   {
@@ -49,7 +20,7 @@ void generateTerranFleet(BattleField *battleField, const char *terranFleetStr) {
 }
 
 void generateProtossFleet(BattleField *battleField, const char *protossFleetStr) {
-  vectorInit(&battleField->protossFleet, sizeof(Vector));
+  vectorInit(&battleField->protossFleet, strlen(protossFleetStr));
   int i = 0;
   while (protossFleetStr[i] != '\0')
   {
