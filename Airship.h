ff2b311620f7389e166da7b564ffa5041ffcd893
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-03-25 16:35:08 +0200

    Update Airship.h

--- Airship.h
+++ Airship.h
@@ -1,1 +1,16 @@
 
+#ifndef Airship_H_
+#define Airship_H_
+#include "Defines.h"
+
+typedef struct {
+  enum AirShipType type;
+  int health;
+  int demage;
+  int shield;
+  int shieldRegenerateRate;
+} Airship;
+
+void initAirship(char c, Airship* ship);
+
+#endif
