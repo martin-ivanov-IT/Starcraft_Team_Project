eb3c5cfbe073bafdf24d3cfef4c3213ac00e25d9
martin-ivanov-IT <mar7i798@gmail.com>
2021-03-28 22:24:32 +0200

    final main.c

--- TerranAirship.h
+++
@@ -1,8 +0,0 @@
-#include "Defines.h"
-#include "Airships.h"
-void initViking(TerranAirship* viking);
-void initBattleCruser(TerranAirship* battleCruser);
-void initTerranAirship(TerranAirship** terranAirship, char letter);
-void TerranAtack(ProtossAirship* protossAirship, TerranAirship* terranAirship, int turn);
-void produceDamage(ProtossAirship* protossAirship, int demage);
-bool isDamagedTerran(TerranAirship* terranAirship);
